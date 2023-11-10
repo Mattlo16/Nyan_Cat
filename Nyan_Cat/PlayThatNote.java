9fd7a6879a4af5c0e83f056bae640d128c680a1a
Matthew Loaces <73414121+Mattlo16@users.noreply.github.com>
2023-11-09 23:22:11 -0500

    Add files via upload

---
+++ Nyan_Cat/PlayThatNote.java
@@ -0,0 +1,25 @@
+public class PlayThatNote {
+    public static double[] tone(double hz, double duration)
+    {
+        int N = (int)(44100 * duration);
+        double[] a = new double[N + 1];
+        for (int i = 0; i <=N; i++)
+        {
+            a[i] = Math.sin(2 * Math.PI * i * hz / 44100);
+        }
+        return a;
+    }
+
+    public static void main(String[] args)
+    {
+        double hz = Double.parseDouble(args[0]);
+        double duration = Double.parseDouble(args[1]);
+        double[] a = tone(hz, duration);
+        StdAudio.play(a);
+
+        
+    }
+
+}
+//Run java PlayThatNote {Tone Frequency(hz)} {time in seconds}
+//java PlayThatNote 440.0 3.0 (Plays )
